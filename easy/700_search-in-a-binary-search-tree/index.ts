044b7c1535a07a35d59835a66464ae6a9a584b50
Nikita Lazurenko <39728010+LazurenkoNA@users.noreply.github.com>
2022-04-14 22:50:05 +0300

    feat: 700 (#2)

---
+++ easy/700_search-in-a-binary-search-tree/index.ts
@@ -0,0 +1,23 @@
+import { arg1, TreeNode } from "./data.js";
+
+function searchBST(root: TreeNode | null, val: number): TreeNode | null {
+  if (!root) return null;
+  if (root.val === val) return root;
+  if (val > root.val) return searchBST(root.right, val);
+  if (val < root.val) return searchBST(root.left, val);
+}
+
+const result = {
+  1: searchBST(arg1, 2),
+  2: searchBST(arg1, 5),
+};
+
+// {
+//   '1': TreeNode {
+//     val: 2,
+//     left: TreeNode { val: 1, left: null, right: null },
+//     right: TreeNode { val: 3, left: null, right: null }
+//   },
+//   '2': null
+// }
+console.log(result);
