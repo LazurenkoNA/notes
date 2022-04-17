c786275c15d4d4258f026ff02cfb8c45998bc553
Nikita Lazurenko <nikita.lazurenko.d@gmail.com>
2022-04-17 23:28:10 +0300

    feat: 897

---
+++ tasks/easy/897.increasing-order-search-tree/index.ts
@@ -0,0 +1,33 @@
+import { arg1, arg2, TreeNode } from "./data.js";
+
+const fillingStack = (root: TreeNode | null, stack: number[]) => {
+  if (!root) return;
+
+  fillingStack(root.left, stack);
+  stack.push(root.val);
+  fillingStack(root.right, stack);
+
+  return stack;
+};
+
+const increasingBST = (root: TreeNode | null): TreeNode | null => {
+  const stack = fillingStack(root, []);
+
+  const head = new TreeNode();
+  let temp = head;
+
+  console.log(stack);
+
+  while (stack.length) {
+    temp.right = new TreeNode(stack.shift());
+    temp = temp.right;
+  }
+
+  return head.right;
+};
+
+const result = {
+  1: increasingBST(arg1),
+  2: increasingBST(arg2),
+};
+console.log(result);
